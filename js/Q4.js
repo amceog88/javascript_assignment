15e321400cd9466127b5291e9fc22a020ac01849
Siegmund <amceog88@gmail.com>
2018-01-17 11:45:29 +0800

    Q4 done

---
+++ js/Q4.js
@@ -0,0 +1,45 @@
+
+
+function createToDo(){
+  var todo = document.createElement("div");
+  var span = document.createElement("span");
+
+  var input = document.getElementById("input").value;
+  if (input == "") {
+    input = "廢文一篇";
+  }
+  span.innerHTML = input;
+  todo.appendChild(span);
+
+/* replace */
+  var replaceButton = document.createElement("button");
+  replaceButton.onclick = function() {
+    var input = document.getElementById("input").value;
+    if( input == "" ) {
+      alert("你並沒有輸入任何文字");
+      return;
+    }
+    this.parentNode.firstChild.innerHTML = input;
+    document.getElementById("input").value = ""; 
+  }
+  replaceButton.textContent = "R";
+  todo.appendChild(replaceButton);
+
+
+/* remove */
+  var removeButton = document.createElement("button");
+  removeButton.onclick = function() {
+    var check = confirm("是否已完成這個事項？"); 
+    if(!check){
+      return;
+    }
+    this.parentNode.firstChild.style.color = "gray";
+    this.parentNode.removeChild(replaceButton);
+    this.parentNode.removeChild(removeButton);
+  }
+  removeButton.textContent = "V";
+  todo.appendChild(removeButton);
+
+  document.getElementById("todolist").appendChild(todo);
+  document.getElementById("input").value = "";
+}
